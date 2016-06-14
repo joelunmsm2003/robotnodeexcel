0a0fb0babe2fad39394f3cb5a4c6bbe17417c3e4
Your Name <you@example.com>
2016-06-13 22:42:28 -0400

    joel

--- lanza.js
+++ lanza.js
@@ -2,7 +2,7 @@ var firebase = require('firebase');
 var http = require('http');
 
 firebase.initializeApp({
-  serviceAccount: "/home/andiu/Escritorio/monitoreo.json",
+  serviceAccount: "/var/www/monitoreo.json",
   databaseURL: "https://monitoreo.firebaseio.com"
 });
 
@@ -10,6 +10,7 @@ firebase.initializeApp({
 var db = firebase.database();
 
 var ref = db.ref("/gamarra/codigo");
+var modelos = db.ref("/gamarra/modelos");
 
 id=2
 
@@ -27,7 +28,8 @@ function addregister(id,talla,color,modelo,ubicacion,precio,estado,fingreso){
 	  ubicacion:ubicacion,
 	  fingreso:fingreso,
 	  estado:estado,
-	  excel:'0'
+	  excel:'0',
+    venta:true
 
 	})
 
@@ -41,7 +43,50 @@ ref.once("value", function(snapshot) {
 var currentTime = new Date();
 
 
-   http.get('http://localhost:8000/nuevos/', function(res){
+      var agregarmodelo =function(model){
+
+        console.log('Agregando modelo....')
+
+        con = 0
+
+        ref.orderByChild("modelo").equalTo(model).on("child_added", function(f) {
+
+        con = con+1
+
+       });
+
+
+
+       setTimeout(function(){ 
+
+      console.log(con)
+      
+      if (con == 1){
+
+          var refmodelo = modelos.child(model);
+       
+
+          refmodelo.set({
+
+              modelo:model
+             
+           
+          })
+
+      }
+      else{
+
+        console.log('Modelo existente...')
+      }
+
+      }, 5000);
+
+
+
+       }
+
+
+   http.get('http://192.241.255.109:1000/nuevos/', function(res){
 
         var str = '';
 
@@ -57,9 +102,10 @@ var currentTime = new Date();
             for( var key in object ) {
  				
  				console.log(object[key])
+        agregarmodelo(object[key][1])
  				addregister(object[key][0],object[key][2],object[key][3],object[key][1],object[key][4],object[key][5],object[key][6],currentTime)
- 		     
- 				http.get('http://localhost:8000/enventa/'+object[key][0]+'/', function(res){})
+        
+ 				http.get('http://192.241.255.109:1000/enventa/'+object[key][0]+'/', function(res){})
 
  		     }
 
@@ -80,7 +126,7 @@ ref.on("child_added", function(snapshot, prevChildKey) {
 
   	if(newPost['excel']=='0'){
 
-  	http.get('http://localhost:8000/vendido/'+newPost['id']+'/', function(res){})
+  	http.get('http://192.241.255.109:1000/vendido/'+newPost['id']+'/', function(res){})
 
   	}
 
